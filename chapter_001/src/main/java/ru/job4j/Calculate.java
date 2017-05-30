5fe7ff40200ca319999059a4ec8cad7590096fb6
Dmitry Belokursky <dbelokursky@gmail.com>
2017-05-30 23:09:44 +0500

    task #4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,27 @@
 package ru.job4j;
 
+/**
+* Class Calculate решение задачи части 001 урок1.
+* @author dbelokursky
+* @since 30.05.2017
+*/
 public class Calculate {
+
+/**
+* Метод для запуска приложения.
+* @param args массив строк с параметрами запуска.
+*/
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		echo("test");
+}
+
+/**
+* Метод для тестирования.
+* @param value строка для вывода в консоль.
+* @return String value.
+*/
+public final String echo(final String value) {
+	return String.format("$s $s $s", value, value, value);
 }
 }
 
