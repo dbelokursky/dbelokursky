a573161dc14fa1af4e697aea67775f29ba182e1d
Dmitry Belokursky <dbelokursky@gmail.com>
2017-06-04 19:09:30 +0500

    Elementary calculator

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,24 +1,24 @@
 package ru.job4j;
 
 /**
-* Class Calculate решение задачи части 001 урок1.
-* @author dbelokursky
-* @since 0.1
+*Class Calculate решение задачи части 001 урок1.
+*@author dbelokursky
+*@since 0.1
 */
 public class Calculate {
 
 /**
-* Метод для запуска приложения.
-* @param args массив строк с параметрами запуска.
+*Метод для запуска приложения.
+*@param args массив строк с параметрами запуска.
 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 }
 
 /**
-* Метод для тестирования.
-* @param value строка для вывода в консоль.
-* @return String value.
+*Метод для тестирования.
+*@param value строка для вывода в консоль.
+*@return String value.
 */
 public final String echo(final String value) {
 	return String.format("$s $s $s", value, value, value);
