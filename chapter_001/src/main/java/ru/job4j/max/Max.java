30963b750cc84a26e90783ae894e4952fde2ae76
Dmitry Belokursky <dbelokursky@gmail.com>
2017-06-04 22:33:24 +0500

    3.1. Максимум из двух чисел

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,17 @@
+package ru.job4j.max;
+
+/**
+* Class Max для решения задачи нахождения максимума из двух чисел.
+*/
+public class Max {
+
+	/**
+	* Метод сравнивает два числа и возвращает большее.
+	* @param first **первое число**
+	* @param second **второе число**
+	* @return max **максимальное число**
+	*/
+	public int max(int first, int second) {
+		return (first > second) ? first : second;
+	}
+}
---
+++ chapter_001/src/main/java/ru/job4j/max/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for the solution of the problem of nding the maximum of two numbers.
+*
+* @author Dmitry Belokursky (mailto:dbelokursky@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.max;
