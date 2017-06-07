a32a9b991977de79ea0c5a41e9c9be6699121f1b
Dmitry Belokursky <dbelokursky@gmail.com>
2017-06-07 18:09:15 +0500

    Исправлен метод area

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -36,18 +36,25 @@ public class Triangle {
 	/**
 	* Метод вычисляет площадь прямоугольника на плоскости.
 	* Формула: s = 1/2*((x1 - x3)*(y1 - y3) - (x2 - x3)*(y1 - y3))
-	* @return result **площадь треугольника**
+	* @return result **площадь треугольника. -1 если через заданные вершины треугольник построить нельзя**
 	*/
 	public double area() {
 		double result;
 		double x1 = this.a.getX();
-		System.out.println(x1);
 		double x2 = this.b.getX();
 		double x3 = this.c.getX();
 		double y1 = this.a.getY();
 		double y2 = this.b.getY();
 		double y3 = this.c.getY();
-		result = 0.5 * ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3));
+		double ab = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		double bc = Math.sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+		double ac = Math.sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+
+        if ((ab + bc) > ac && (bc + ac) > ab && (ab + ac) > ab) {
+        	result = Math.abs(0.5 * ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)));
+        } else {
+        	result = -1;
+        }
 		return result;
 	}
 }
