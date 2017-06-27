cd177735048cdc5e9eab1f2762589e775f49673d
Dmitry Belokursky <dbelokursky@gmail.com>
2017-06-27 20:37:22 +0500

    1. Реализация профессий в коде [#6837]

--- chapter_002/src/main/java/ru/job4j/inheritance/Engineer.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/Engineer.java
@@ -1,10 +1,15 @@
 package ru.job4j.inheritance;
 
 /**
- * Created by db on 26.06.17.
+ * Class Engineer. Характеристики инженера.
  */
 public class Engineer extends Profession {
 
+    /**
+     * Метод возвращает фразу содержащую ФИО инженера и название оборудования.
+     * @param equipment **ремонтируемое оборудование**
+     * @return **фраза содержащая ФИО инженера и название оборудования**
+     */
     public String repairEquipment(Equipment equipment) {
         return (String.format("Engineer $s repair $s", this.getFullName(), equipment.getName()));
     }
