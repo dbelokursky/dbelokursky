1b57a551f0dc7a6ae62cb9e3316e5a49379176e0
Dmitry Belokursky <dbelokursky@gmail.com>
2017-06-27 20:55:43 +0500

    1. Реализация профессий в коде [#6837]

--- chapter_002/src/main/java/ru/job4j/inheritance/Profession.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/Profession.java
@@ -1,46 +1,90 @@
 package ru.job4j.inheritance;
 
 /**
- * Created by db on 26.06.17.
+ * Class Profession. Хранит характеристики профессии.
  */
 public class Profession {
 
+    /**
+     * Поле хранит ФИО
+     */
     private String fullName;
 
+    /**
+     * Поле хранит название специализации.
+     */
     private String specialization;
 
+    /**
+     * Поле хранит стаж(количество лет).
+     */
     private int experience;
 
+    /**
+     * Поле хранит значение оклада.
+     */
     private int salary;
 
+    /**
+     * Метод устанавливает ФИО.
+     * @param fullName **ФИО**
+     */
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
+    /**
+     * Метод устанавливает название специализации.
+     * @param specialization **название специализации**
+     */
     public void setSpecialization(String specialization) {
         this.specialization = specialization;
     }
 
+    /**
+     * Метод устанавливает значение стажа.
+     * @param experience **стаж**
+     */
     public void setExperience(int experience) {
         this.experience = experience;
     }
 
+    /**
+     * Метод устанавливает значение оклада.
+     * @param salary **оклад**
+     */
     public void setSalary(int salary) {
         this.salary = salary;
     }
 
+    /**
+     * Метод возвращает значение ФИО.
+     * @return **ФИО**
+     */
     public String getFullName() {
         return fullName;
     }
 
+    /**
+     * Метод возвращает значение специализации.
+     * @return **специализация**
+     */
     public String getSpecialization() {
         return specialization;
     }
 
+    /**
+     * Метод возвращает значение стажа.
+     * @return **стаж**
+     */
     public int getExperience() {
         return experience;
     }
 
+    /**
+     * Метод возвращает значение оклада.
+     * @return **оклад**
+     */
     public int getSalary() {
         return salary;
     }
