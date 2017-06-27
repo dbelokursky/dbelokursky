ebd06818a5a47ba141c5513b135ba7b951b73d15
Dmitry Belokursky <dbelokursky@gmail.com>
2017-06-27 22:11:08 +0500

    Add package-info Реализация профессий в коде [#6837]

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for inheritance task.
+*
+* @author Dmitry Belokursky (mailto:dbelokursky@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.inheritance;
---
+++ chapter_002/src/test/java/ru/job4j/inheritance/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for testing inheritance task.
+*
+* @author Dmitry Belokursky (mailto:dbelokursky@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.inheritance;
+
