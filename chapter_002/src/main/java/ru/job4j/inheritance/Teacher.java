515aaa821e321217e7bcf9fb3b75ab6fa63eccc5
Dmitry Belokursky <dbelokursky@gmail.com>
2017-06-27 21:06:14 +0500

    1. Реализация профессий в коде [#6837]

--- chapter_002/src/main/java/ru/job4j/inheritance/Teacher.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/Teacher.java
@@ -3,15 +3,27 @@ package ru.job4j.inheritance;
 import java.util.Date;
 
 /**
- * Created by db on 26.06.17.
+ * Class Teacher. Хранит характеристики учителя.
  */
 public class Teacher extends Profession {
 
+    /**
+     * Метод возвращает фразу содержащую ФИО учителя, название дисциплины и номер аудитории.
+     * @param audienceNumber **номер аудитории**
+     * @param discipline **название дисциплины**
+     * @return **фраза содержащая ФИО учителя, название дисциплины и номер аудитории**
+     */
     public String teach(int audienceNumber, Discipline discipline) {
         return (String.format("Teacher $s teach $ in audience №$s", this.getFullName(), discipline.getName(), audienceNumber));
     }
 
+    /**
+     * Метод возвращает фразу содержащую ФИО учителя, количество дней и дату начала отпуска.
+     * @param numberOfDays **количество дней**
+     * @param startDate **дата начала отпуска**
+     * @return **фраза содержащая ФИО учителя, количество дней и дату начала отпуска.**
+     */
     public String goOnVacation(int numberOfDays, Date startDate) {
-        return (String.format("$ on vacation duration $s days since $s", this.getFullName(), numberOfDays, startDate));
+        return (String.format("$ on vacation duration $s days since $s", this.getFullName(), numberOfDays, startDate.toString()));
     }
 }
