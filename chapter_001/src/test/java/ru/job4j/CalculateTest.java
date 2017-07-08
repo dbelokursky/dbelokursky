08d35ea0ea13460827865b15e4b0cb028e1f77d8
Dmitry Belokursky <dbelokursky@gmail.com>
2017-07-08 19:47:33 +0500

    Calculate ref

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -1,8 +1,10 @@
 package ru.job4j;
 
 import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -22,14 +24,6 @@ public class CalculateTest {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
         Calculate.main(null);
-        assertThat(
-                out.toString(),
-                is(
-                        String.format(
-                                "Hello World%s",
-                                System.getProperty("line.separator")
-                        )
-                )
-        );
+        assertThat(out.toString(), is(String.format("Hello World%s", System.getProperty("line.separator"))));
     }
 }
