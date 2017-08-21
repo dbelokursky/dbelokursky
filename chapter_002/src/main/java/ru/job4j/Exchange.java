a579dd6cfc137789c3463f4837a25defbe9c1fb3
Dmitry Belokursky <dbelokursky@gmail.com>
2017-08-21 21:49:40 +0500

    Размен купюры.

--- chapter_002/src/main/java/ru/job4j/Exchange.java
+++ chapter_002/src/main/java/ru/job4j/Exchange.java
@@ -1,34 +1,37 @@
 package ru.job4j;
 
-/**
- * @author Dmitry Belokursky
- * @since 13.08.17.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exchange {
 
-    /**
-     * Предлагает варианты размена купюры монетами номиналом 1, 2, 5, 10 рублей.
-     *
-     * @param den номинал купюры(10, 50, 100, 500, 1000, 5000).
-     * @return строку с вариантами обмена.
-     */
-    public String change(int den) throws IllegalArgumentException {
-        String result = "";
-        if (den == 10 || den == 50 || den == 100 || den == 500 || den == 1000 || den == 5000) {
-            int numberOfOneRubleCoins = den / 1;
-            int numberOfTwoRubleCoins = den / 2;
-            int numberOfFiveRubleCoins = den / 5;
-            int numberOfTenRubleCoins = den / 10;
-            StringBuilder sb = new StringBuilder();
-            String lineSeparator = System.getProperty("line.separator");
-            sb.append(numberOfOneRubleCoins).append(" монет номиналом 1 рубль").append(lineSeparator)
-                    .append(numberOfTwoRubleCoins).append(" монет номиналом 2 рубля").append(lineSeparator)
-                    .append(numberOfFiveRubleCoins).append(" монет номиналом 5 рублей").append(lineSeparator)
-                    .append(numberOfTenRubleCoins).append(" монет номиналом 10 рублей");
-            result = sb.toString();
-        } else {
-            throw new IllegalArgumentException("Номинал купюры может быть: 10, 50, 100, 500, 1000, 5000.");
+    private List<Integer> waysOfExchange;
+
+    private StringBuilder textRepresentation;
+
+    public Exchange() {
+        this.waysOfExchange = new ArrayList<>();
+        this.textRepresentation = new StringBuilder();
+    }
+
+    public String getExchangeWays(int denomination, int coins[]) {
+        calculateExchangeWays(denomination, coins, 0);
+        return textRepresentation.toString();
+    }
+
+    private void calculateExchangeWays(int denomination, int coins[], int position) {
+        if (denomination == 0) {
+            for (int r : this.waysOfExchange) {
+                this.textRepresentation.append(r).append(" ");
+            }
+            this.textRepresentation.append(System.getProperty("line.separator"));
         }
-        return result;
+        for (int i = position; i < coins.length; i++) {
+            if (denomination >= coins[i]) {
+                this.waysOfExchange.add(coins[i]);
+                calculateExchangeWays(denomination - coins[i], coins, i);
+                this.waysOfExchange.remove(this.waysOfExchange.size() - 1);
+            }
+        }
     }
 }
--- chapter_002/src/test/java/ru/job4j/ExchangeTest.java
+++ chapter_002/src/test/java/ru/job4j/ExchangeTest.java
@@ -1,24 +1,25 @@
 package ru.job4j;
 
+/**
+ * @author Dmitry Belokursky
+ * @since 21.08.17.
+ */
+
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-/**
- * @author Dmitry Belokursky
- * @since 13.08.17.
- */
 public class ExchangeTest {
 
     @Test
-    public void changeTest() {
+    public void getExchangeWaysTest() {
         Exchange ex = new Exchange();
-        String result = ex.change(50);
-        String expected = "50 монет номиналом 1 рубль\n" +
-                "25 монет номиналом 2 рубля\n" +
-                "10 монет номиналом 5 рублей\n" +
-                "5 монет номиналом 10 рублей";
+        String expected = "1 1 1 1 1 1 1 1 1 1 \n" +
+                "1 1 1 1 1 5 \n" +
+                "5 5 \n" +
+                "10 \n";
+        String result = ex.getExchangeWays(10, new int[]{1, 5, 10});
         assertThat(result, is(expected));
     }
 }
