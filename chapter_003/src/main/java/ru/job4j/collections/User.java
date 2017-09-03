52773fb8d2375226cf437c91be3a070055d10d2c
Dmitry Belokursky <dbelokursky@gmail.com>
2017-09-03 12:56:37 +0500

    2. Написать программу преобразования List в Map.

---
+++ chapter_003/src/main/java/ru/job4j/collections/User.java
@@ -0,0 +1,52 @@
+package ru.job4j.collections;
+
+/**
+ * @author Dmitry Belokursky
+ * @since 03.09.17.
+ */
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private String city;
+
+    public User(int id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (getId() != user.getId()) return false;
+        if (getName() != null ? !getName().equals(user.getName()) : user.getName() != null) return false;
+        return getCity() != null ? getCity().equals(user.getCity()) : user.getCity() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getCity() != null ? getCity().hashCode() : 0);
+        return result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/collections/UserConvert.java
@@ -0,0 +1,19 @@
+package ru.job4j.collections;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author Dmitry Belokursky
+ * @since 03.09.17.
+ */
+public class UserConvert {
+
+    public HashMap<Integer, User> process(List<User> list) {
+        HashMap<Integer, User> result = new HashMap<>();
+        for (User user : list) {
+            result.put(user.getId(), user);
+        }
+        return result;
+    }
+}
