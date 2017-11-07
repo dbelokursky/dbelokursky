e7de74df7ec67450cd55ff7861ce31e3a984af2d
Dmitry Belokursky <dbelokursky@gmail.com>
2017-11-07 20:27:39 +0500

    3. Реализовать коллекцию типа Set на базе хэш-таблицы [#998]

---
+++ chapter_005/src/main/java/ru/job4j/set/SimpleHashSet.java
@@ -0,0 +1,40 @@
+package ru.job4j.set;
+
+import java.util.HashMap;
+
+/**
+ * @author Dmitry Belokursky
+ * @since 07.11.17.
+ */
+public class SimpleHashSet<E> {
+
+    private HashMap<Integer, E> container;
+
+    public SimpleHashSet() {
+        this.container = new HashMap<>(16);
+    }
+
+    boolean add(E e) {
+        boolean result = false;
+        if (!contains(e)) {
+            container.put(e.hashCode(), e);
+            result = true;
+        }
+        return result;
+    }
+
+    boolean contains(E e) {
+        return container.containsValue(e);
+    }
+
+    boolean remove(E e) {
+        return container.remove(e.hashCode(), e);
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleHashSet{" +
+                "container=" + container +
+                '}';
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/set/SimpleHashSetTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Dmitry Belokursky
+ * @since 07.11.17.
+ */
+public class SimpleHashSetTest {
+
+    SimpleHashSet<String> simpleHashSet = new SimpleHashSet<>();
+
+    @Before
+    public void before() {
+        simpleHashSet.add("first");
+        simpleHashSet.add("second");
+        simpleHashSet.add("third");
+        System.out.println(simpleHashSet);
+    }
+
+    @Test
+    public void add() throws Exception {
+        boolean result = simpleHashSet.contains("second");
+        boolean expected = true;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void contains() throws Exception {
+        boolean result = simpleHashSet.contains("first");
+        boolean expected = true;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void remove() throws Exception {
+        simpleHashSet.remove("first");
+        boolean result = simpleHashSet.contains("first");
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+}
