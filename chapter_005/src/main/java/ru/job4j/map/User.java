afa86ac5e7be283f58c772fce138115e1aeda735
Dmitry Belokursky <dbelokursky@gmail.com>
2017-11-07 22:54:27 +0500

    1. Создать модель User[#30301]

---
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,16 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * @author Dmitry Belokursky
+ * @since 07.11.17.
+ */
+public class User {
+
+    private String name;
+
+    private int children;
+
+    private Calendar birthday;
+}
