72368fb9774a6946f357437b6091b74f93e14cf5
Dmitry Belokursky <dbelokursky@gmail.com>
2018-07-01 10:56:26 +0500

    2. Лямбды и шаблон wrapper.[#58661] fix

--- chapter_009/01/src/main/java/ru/job4j/todo/Item.java
+++ chapter_009/01/src/main/java/ru/job4j/todo/Item.java
@@ -1,11 +1,21 @@
 package ru.job4j.todo;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.sql.Timestamp;
 
 /**
  * @author Dmitry Belokursky
  * @since 10.06.18.
  */
+
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Item {
 
     private int id;
@@ -15,72 +25,4 @@ public class Item {
     private Timestamp created;
 
     private boolean done;
-
-    public Item() {
-
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Timestamp getCreated() {
-        return created;
-    }
-
-    public void setCreated(Timestamp created) {
-        this.created = created;
-    }
-
-    public boolean isDone() {
-        return done;
-    }
-
-    public void setDone(boolean done) {
-        this.done = done;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Item item = (Item) o;
-
-        if (getId() != item.getId()) {
-            return false;
-        }
-        if (isDone() != item.isDone()) {
-            return false;
-        }
-        if (!getDescription().equals(item.getDescription())) {
-            return false;
-        }
-        return getCreated().equals(item.getCreated());
-    }
-
-    @Override
-    public int hashCode() {
-        int result = getId();
-        result = 31 * result + getDescription().hashCode();
-        result = 31 * result + getCreated().hashCode();
-        result = 31 * result + (isDone() ? 1 : 0);
-        return result;
-    }
 }
--- chapter_009/01/src/main/java/ru/job4j/todo/ItemsStore.java
+++ chapter_009/01/src/main/java/ru/job4j/todo/ItemsStore.java
@@ -1,24 +1,22 @@
 package ru.job4j.todo;
 
-import org.apache.log4j.Logger;
+import lombok.extern.log4j.Log4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
  * @author Dmitry Belokursky
  * @since 10.06.18.
  */
+@Log4j
 public enum ItemsStore {
 
     INSTANCE;
-
-    private static final Logger LOGGER = Logger.getLogger("ItemsStore.class");
 
     private static SessionFactory sessionFactory;
 
@@ -27,37 +25,14 @@ public enum ItemsStore {
     }
 
     public void add(Item item) {
-        addTransaction((session -> session.save(item)), item);
+        tx(session -> session.save(item));
     }
 
     public List<Item> getAll() {
-        return getAllTransaction(session -> session.createQuery("from Item").list());
+        return tx(session -> session.createQuery("from Item").list());
     }
 
-    public void update(int id, Item newItem) {
-        updateTransaction(session -> session.update(newItem), id, newItem);
-    }
-
-    public void delete(int id) {
-        deleteTransaction(session -> session.delete(id), id);
-
-    }
-
-    private void addTransaction(Consumer<Session> consumer, Item item) {
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            session.save(item);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            LOGGER.error(e.getMessage(), e);
-        }
-    }
-
-    private <R> R getAllTransaction(final Function<Session, R> command) {
+    private <R> R tx(final Function<Session, R> command) {
         Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
@@ -66,37 +41,8 @@ public enum ItemsStore {
             if (transaction != null) {
                 transaction.rollback();
             }
-            LOGGER.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
             throw e;
-        }
-    }
-
-    private void updateTransaction(Consumer<Session> consumer, int id, Item newItem) {
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            newItem.setId(id);
-            session.update(newItem);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            LOGGER.error(e.getMessage(), e);
-        }
-    }
-
-    private void deleteTransaction(Consumer<Session> consumer, int id) {
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            session.delete(id);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            LOGGER.error(e.getMessage(), e);
         }
     }
 }
