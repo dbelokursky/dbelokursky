d531892e08e597fe7e17859365f6291326032e9a
Dmitry Belokursky <dbelokursky@gmail.com>
2018-08-03 14:57:39 +0500

    add Liquibase

--- chapter_009/_02/src/test/java/ru/job4j/carssale/CarsStoreTest.java
+++ chapter_009/_02/src/test/java/ru/job4j/carssale/CarsStoreTest.java
@@ -6,17 +6,19 @@ import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
-import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.junit.Before;
 import org.junit.Test;
-import ru.job4j.carssale.models.*;
+import ru.job4j.carssale.models.Car;
+import ru.job4j.carssale.models.Image;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.is;
@@ -25,7 +27,7 @@ import static org.junit.Assert.assertThat;
 public class CarsStoreTest {
 
 
-    @Test
+    @Before
     public void init() throws SQLException, LiquibaseException {
         // Prepare the Hibernate configuration
         StandardServiceRegistry reg = new StandardServiceRegistryBuilder().configure().build();
@@ -39,89 +41,39 @@ public class CarsStoreTest {
         Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(jdbcCon);
         Liquibase liquibase = new Liquibase("db.changelog-master.xml", new ClassLoaderResourceAccessor(), database);
         liquibase.update("test");
-
-        // Create Hibernate SessionFactory
-        SessionFactory sf = metaDataSrc.addAnnotatedClass(Car.class)
-                .addAnnotatedClass(Engine.class)
-                .addAnnotatedClass(Image.class)
-                .addAnnotatedClass(Owner.class)
-                .addAnnotatedClass(Suspension.class)
-                .addAnnotatedClass(Transmission.class)
-                .buildMetadata().buildSessionFactory();
     }
 
     @Test
-    public void add() throws LiquibaseException, SQLException {
-        // Prepare the Hibernate configuration
-        StandardServiceRegistry reg = new StandardServiceRegistryBuilder().configure().build();
-        MetadataSources metaDataSrc = new MetadataSources(reg);
-
-        // Get database connection
-        Connection con = metaDataSrc.getServiceRegistry().getService(ConnectionProvider.class).getConnection();
-        JdbcConnection jdbcCon = new JdbcConnection(con);
-
-        // Initialize Liquibase and run the update
-        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(jdbcCon);
-        Liquibase liquibase = new Liquibase("db.changelog-master.xml", new ClassLoaderResourceAccessor(), database);
-        liquibase.update("test");
-
-        // Create Hibernate SessionFactory
-        SessionFactory sf = metaDataSrc.addAnnotatedClass(Car.class)
-                .addAnnotatedClass(Engine.class)
-                .addAnnotatedClass(Image.class)
-                .addAnnotatedClass(Owner.class)
-                .addAnnotatedClass(Suspension.class)
-                .addAnnotatedClass(Transmission.class)
-                .buildMetadata().buildSessionFactory();
+    public void add() {
         CarsStore carsStore = new CarsStore();
         Car car = new Car();
         Set<Image> images = new HashSet<>();
         car.setModel("testModel");
         car.setBrand("testBrand");
         car.setImages(images);
-
         carsStore.add(car, images);
-
         assertThat(car, is(carsStore.getCar(car.getId())));
     }
 
     @Test
     public void update() {
         CarsStore carsStore = new CarsStore();
-        Car car = new Car();
-        Set<Image> images = new HashSet<>();
-        car.setSold(false);
-        car.setModel("testModel");
-        car.setBrand("testBrand");
-        car.setImages(images);
+        Car car = carsStore.getCar(1);
+        car.setModel("modelUpdated");
+        car.setBrand("brandUpdated");
         car.setSold(true);
-        carsStore.add(car, images);
         carsStore.update(car);
 
-        boolean expected = true;
-        boolean result = carsStore.getCar(car.getId()).isSold();
+        String expected = "modelUpdated";
+        String result = carsStore.getCar(1).getModel();
+        List<Car> cars = carsStore.getAll();
         assertThat(result, is(expected));
     }
 
     @Test
     public void getAll() {
         CarsStore carsStore = new CarsStore();
-        Car car1 = new Car();
-        Set<Image> images = new HashSet<>();
-        car1.setSold(false);
-        car1.setModel("testModel");
-        car1.setBrand("testBrand");
-        car1.setImages(images);
-        Car car2 = new Car();
-        car2.setSold(false);
-        car2.setModel("testModel2");
-        car2.setBrand("testBrand2");
-        car2.setImages(images);
-
-        carsStore.add(car1, images);
-        carsStore.add(car2, images);
-
-        int expected = 2;
+        int expected = 5;
         int result = carsStore.getAll().size();
         assertThat(result, is(expected));
     }
@@ -129,18 +81,8 @@ public class CarsStoreTest {
     @Test
     public void getCar() {
         CarsStore carsStore = new CarsStore();
-        Car car = new Car();
-        Set<Image> images = new HashSet<>();
-        car.setSold(false);
-        car.setModel("testModel4");
-        car.setBrand("testBrand4");
-        car.setImages(images);
-
-        carsStore.add(car, images);
-
-        String expected = "testModel4";
-        String result = carsStore.getCar(car.getId()).getModel();
+        String expected = "model1";
+        String result = carsStore.getCar(1).getModel();
         assertThat(result, is(expected));
-
     }
 }
