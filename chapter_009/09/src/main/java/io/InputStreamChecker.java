2e38be4dac9d278099ffbb3bdb7a180e04552712
Dmitry Belokursky <dbelokursky@gmail.com>
2018-11-11 20:43:47 +0500

    2. Удаление запрещенных слов[#86016]

--- chapter_009/09/src/main/java/io/InputStreamChecker.java
+++ chapter_009/09/src/main/java/io/InputStreamChecker.java
@@ -1,8 +1,12 @@
 package io;
 
-import java.io.InputStream;
+import lombok.extern.log4j.Log4j;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+@Log4j
 public class InputStreamChecker {
 
     boolean isEven(InputStream inputStream) {
@@ -18,4 +22,18 @@ public class InputStreamChecker {
         return result;
     }
 
+    void dropAbusive(InputStream is, OutputStream os, String[] abusiveWords) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                for (String word : abusiveWords) {
+                    line = line.replaceAll(word, "");
+                }
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(os));
+                bufferedWriter.write(line);
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
 }
